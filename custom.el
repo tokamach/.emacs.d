51310c15bdd0483049a4f43d9e810032bcbc3a72
Tom Hutchings <goerofmuns@gmail.com>
2018-03-01 11:41:45 +0000

    move kawaiify theme system to own file

--- custom.el
+++ custom.el
@@ -6,20 +6,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#ffffff" "#d8137f" "#17ad98" "#dc8a0e" "#775dff" "#aa17e6" "#775dff" "#6c696e"])
+ '(ansi-term-color-vector
+   [unspecified "#ffffff" "#d8137f" "#17ad98" "#dc8a0e" "#775dff" "#aa17e6" "#775dff" "#6c696e"] t)
  '(blink-cursor-mode nil)
  '(company-clang-arguments (quote ("-I/usr/local/include/" "--std=c++11")))
  '(custom-safe-themes
    (quote
-    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "f2057733672d3b119791f5b7d1a778bf8880121f22ea122a21d221b45081f49e" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "a85e40c7d2df4a5e993742929dfd903899b66a667547f740872797198778d7b5" "4bf5c18667c48f2979ead0f0bdaaa12c2b52014a6abaa38558a207a65caeb8ad" "8cf1002c7f805360115700144c0031b9cfa4d03edc6a0f38718cef7b7cabe382" "60e09d2e58343186a59d9ed52a9b13d822a174b33f20bdc1d4abb86e6b17f45b" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "ffe80c88e3129b2cddadaaf78263a7f896d833a77c96349052ad5b7753c0c5a5" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "0ee3fc6d2e0fc8715ff59aed2432510d98f7e76fe81d183a0eb96789f4d897ca" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a1382d7377687da344ab290dc6718839031ddd5989cb1e756f7f7d575201ea63" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" "10e231624707d46f7b2059cc9280c332f7c7a530ebc17dba7e506df34c5332c4" "47aa6e82734866b2915781c6e1d9517bd897d45fe8aec360dd4b6294fec73068" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" default)))
+    ("ad16a1bf1fd86bfbedae4b32c269b19f8d20d416bd52a87cd50e355bf13c2f23" "d83e34e28680f2ed99fe50fea79f441ca3fddd90167a72b796455e791c90dc49" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "f2057733672d3b119791f5b7d1a778bf8880121f22ea122a21d221b45081f49e" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "a85e40c7d2df4a5e993742929dfd903899b66a667547f740872797198778d7b5" "4bf5c18667c48f2979ead0f0bdaaa12c2b52014a6abaa38558a207a65caeb8ad" "8cf1002c7f805360115700144c0031b9cfa4d03edc6a0f38718cef7b7cabe382" "60e09d2e58343186a59d9ed52a9b13d822a174b33f20bdc1d4abb86e6b17f45b" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "ffe80c88e3129b2cddadaaf78263a7f896d833a77c96349052ad5b7753c0c5a5" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "0ee3fc6d2e0fc8715ff59aed2432510d98f7e76fe81d183a0eb96789f4d897ca" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a1382d7377687da344ab290dc6718839031ddd5989cb1e756f7f7d575201ea63" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" "10e231624707d46f7b2059cc9280c332f7c7a530ebc17dba7e506df34c5332c4" "47aa6e82734866b2915781c6e1d9517bd897d45fe8aec360dd4b6294fec73068" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" default)))
  '(electric-pair-mode t)
  '(markdown-command "/usr/local/bin/markdown")
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (rust-mode all-the-icons svg-clock tao-theme evil-org evil-vimish-fold exec-path-from-shell arduino-mode evil-magit flycheck json-mode buffer-move yaml-mode smooth-scrolling xah-fly-keys irony company-jedi sudoku gnugo symon slack rainbow-delimiters smart-mode-line-powerline-theme smart-mode-line neotree imenu-list xcode-mode swift3-mode auctex base16-theme lua-mode web-mode zenburn-theme solarized-theme birds-of-paradise-plus-theme fill-column-indicator projectile minimap company-auctex latex-pretty-symbols gruvbox-theme slime-company slime which-key markdown-preview-mode smart-tabs-mode evil-leader xkcd fireplace company parinfer darktooth-theme selectric-mode markdown-mode anti-zenburn-theme magit 2048-game twittering-mode smex ido-vertical-mode olivetti fountain-mode creamsody-theme ## evil-visual-mark-mode)))
+    (mips-mode rust-mode all-the-icons svg-clock tao-theme evil-org evil-vimish-fold exec-path-from-shell arduino-mode evil-magit flycheck json-mode buffer-move yaml-mode smooth-scrolling xah-fly-keys irony company-jedi sudoku gnugo symon slack rainbow-delimiters smart-mode-line-powerline-theme smart-mode-line neotree imenu-list xcode-mode swift3-mode auctex base16-theme lua-mode web-mode zenburn-theme solarized-theme birds-of-paradise-plus-theme fill-column-indicator projectile minimap company-auctex latex-pretty-symbols gruvbox-theme slime-company slime which-key markdown-preview-mode smart-tabs-mode evil-leader xkcd fireplace company parinfer darktooth-theme selectric-mode markdown-mode anti-zenburn-theme magit 2048-game twittering-mode smex ido-vertical-mode olivetti fountain-mode creamsody-theme ## evil-visual-mark-mode)))
+ '(pos-tip-background-color "#1A3734")
+ '(pos-tip-foreground-color "#FFFFC8")
  '(safe-local-variable-values
    (quote
-    ((olivetti-body-width . 94)
+    ((c-default-style . "linux")
+     (olivetti-body-width . 94)
      (olivetti-body-width . 100)
      (company-clang-arguments "-I/usr/local/include/" "-I/usr/local/include/GLFW/" "--std=c++11")
      (company-clang-arguments
--- init.el
+++ init.el
@@ -29,39 +29,9 @@
    ("f" "~/Pictures/animu/re_zero/felix")
    ("p" "~/Pictures/animu/proggybooks")))
 
-;; kawaiify theme
-(require 'cl)
-(require 'filenotify)
-
-(defun load-new-theme (theme)
-  (interactive)
-  (dolist (i custom-enabled-themes)
-    (disable-theme i))
-  (load-theme theme t)
-  (load-theme 'smart-mode-line-respectful t))
-
-(defun get-string-from-file (filePath)
-  "Return filePath's file content."
-  (with-temp-buffer
-    (insert-file-contents filePath)
-    (buffer-string)))
-
-(defun theme-update-callback (event)
-  (pcase (get-string-from-file "~/.kawaiify/theme.lock")
-    ("pink\n"      (load-new-theme 'base16-unikitty-light))
-    ("purpur\n"    (load-new-theme 'base16-paraiso))
-    ("sleeper\n"   (load-new-theme 'adwaita))
-    ("catboi\n"    (load-new-theme 'creamsody) (creamsody-modeline-two))
-    ("blue\n"      (load-new-theme 'creamsody) (creamsody-modeline-two))
-    ("pinkish\n"   (load-new-theme 'birds-of-paradise-plus))
-    ("dark\n"      (load-new-theme 'ciapre))
-    (_             (load-new-theme 'zenburn)))) ;fallback
-
-(theme-update-callback 0)
-(file-notify-add-watch
- "~/.kawaiify/theme.lock"
- '(change attribute-change)
- 'theme-update-callback)
+;; theme
+(load-new-theme 'base16-gruvbox-light-soft)
+(sml/apply-theme 'automatic t)
 
 ;; tramp
 (setq tramp-default-method "ssh")
