012240a70707fd431fbda9a69da071e2356f4f1e
Tom Hutchings <goerofmuns@gmail.com>
2018-07-26 01:52:56 +0100

    get entry-plug up to spec

--- lisp/entry-plug.el
+++ lisp/entry-plug.el
@@ -2,44 +2,51 @@
 ;;;; just get in the fucking text editor shinji
 ;;;; -*- lexical-binding: t; -*-
 
-(defvar entry-plug-mode-map (make-keymap))
+(defvar entry-plug-mode-map (make-sparse-keymap))
+(defvar entry-plug-mode-enabled-p t)
 
 (defcustom entry-plug/leader "<SPC>"
   "Leader key for entry-plug mode"
   :type 'string)
 
-(defun entry-plug--extract-command-or-key (cmd)
-  (cond ((stringp cmd) (key-binding cmd))
-        (t cmd)))
+(defcustom entry-plug/exit-key "<ESC>"
+  "Key to return to entry-plug mode"
+  :type 'string)
 
 (defun entry-plug/set-leader-key (key)
   (setq entry-plug/leader key))
 
-(defun entry-plug/set-root-keymap (list)
-  (dolist (key-elem list)
-    (let ((key (car key-elem))
-          (cmd (nth 1 key-elem)))
-      (define-key entry-plug-mode-map
-                  (kbd key)
-                  (entry-plug--extract-command-or-key cmd)))))
+(defun entry-plug--extract-command-or-key (cmd)
+  (cond ((stringp cmd) (key-binding cmd))
+        (t cmd)))
 
-(defun entry-plug/set-leader-keymap (list)
-  (dolist (key-elem list)
-    (let ((key (car key-elem))
-          (cmd (nth 1 key-elem)))
-      (define-key entry-plug-mode-map
-                  (kbd (concat entry-plug/leader key))
-                  (entry-plug--extract-command-or-key cmd)))))
+(defun entry-plug/set-root-keymap (key cmd &rest bindings)
+  (while key
+    (define-key entry-plug-mode-map
+      (read-kbd-macro key)
+      cmd)
+    (setq key (pop bindings)
+	  cmd (pop bindings))))
+
+(defun entry-plug/set-leader-keymap (key cmd &rest bindings)
+  (while key
+    (define-key entry-plug-mode-map
+      (read-kbd-macro (concat entry-plug/leader key))
+      cmd)
+    (setq key (pop bindings)
+	  cmd (pop bindings))))
 
 (define-minor-mode entry-plug-mode
   "Modal editing mode"
   :lighter " entry-plug"
   :keymap entry-plug-mode-map)
 
-(define-globalized-minor-mode
-  global-entry-plug-mode
-  entry-plug-mode
-  (lambda () (entry-plug-mode 1)))
+;; (define-globalized-minor-mode
+;;   global-entry-plug-mode
+;;   entry-plug-mode
+;;   (lambda ()
+;;     (define-key )
+;;     (entry-plug-mode 1)))
   
 (provide 'entry-plug)
 ;;; entry-plug.el ends here
