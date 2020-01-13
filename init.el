d7a16b420279c953fbcba402512305d36568ee6e
Tom Hutchings <goerofmuns@gmail.com>
2020-01-13 22:25:53 +0000

    fix org autoload issues with straight.el

--- init.el
+++ init.el
@@ -1,3 +1,18 @@
+;; For annoying autoload reasons we have to do this here isntead of in conf.org
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+
 ;; Load the actual configuration
-(require 'org)
 (org-babel-load-file (concat user-emacs-directory "conf.org"))
