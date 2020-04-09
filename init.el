26055a909c220bd571b88db0c188f364626100cd
Tom Hutchings <goerofmuns@gmail.com>
2020-04-09 18:58:07 +0100

    no compilation society has surpassed the need for compilation

--- init.el
+++ init.el
@@ -14,8 +14,4 @@
 
 (straight-use-package 'org)
 
-;; Load a compiled file if exists, otherwise tangle and interpret
-(let ((compiled-conf (expand-file-name "conf.elc" user-emacs-directory)))
-  (if (file-exists-p compiled-conf)
-      (load-file compiled-conf)
-    (org-babel-load-file (concat user-emacs-directory "conf.org"))))
+(org-babel-load-file (concat user-emacs-directory "conf.org"))
