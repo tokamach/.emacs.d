0cd6bb7260cf1ac0e37fa6f3ddbbda3c75fee344
Tom Hutchings <goerofmuns@gmail.com>
2020-05-16 14:56:04 +0100

    cellular automata sim and pdf tools

---
+++ lisp/ca.el
@@ -0,0 +1,59 @@
+;; -*- lexical-binding: t; -*-
+
+(defconst cell-size 2)
+
+(defun make-rule (id)
+  "Make a 1D elementary cellular automata lookup array using Wolfram code ID"
+  ;; Decompose ID to it's bits. Each bit from LSB to MSB represents the result
+  ;; of taking the index of that bit as a lookup trio of cell states.
+  (vconcat (loop for index from 0 upto 7
+		 for bit = (logand 1 id) then (logand 1 (lsh id (- index)))
+		 collect bit)))
+
+(defun rule-lookup (rule a b c)
+  (aref rule (logior (lsh a 2)
+		     (lsh b 1)
+		     c)))
+
+(defun evolve (pop rule)
+  "Take a variable size pop POP and evolve according to RULE."
+  ;; TODO ignore edges i.e. start at 1 and end at len - 1
+  (append '(0) (loop for (a b c) on pop
+		     when (not (eq c nil))
+		     collect (rule-lookup rule a b c))
+	  '(0)))
+
+(defun draw-pop-gen (pop gen)
+  "Draw population on GEN, i.e. GEN times cell-size from origin."
+  (let ((pop-len (length pop)))
+    (loop for cell in pop
+	  for n from 1
+	  when (eq cell 1)
+	  collect `(rect ((x . ,(* n cell-size))
+			  (y . ,(* gen cell-size))
+			  (width . ,cell-size)
+			  (height . ,cell-size))))))
+
+;; TODO random inital pop or single life
+(defun draw-rule (svg rule maxgens &rest pop)
+  "Draw CA rule RULE, with MAXGENS iterations, onto canvas SVG.
+POP specifies a custom population, which defaults to a single 1 in the centre."
+  (let* ((svg-width (cdr (assoc 'width (cadr svg))))
+	 (padding-list (make-list (/ (/ svg-width cell-size) 2) 0))
+	 (pop (when (eq pop nil)
+		(append padding-list '(1) padding-list))))
+    (loop for i from 0 upto maxgens
+	  for iter-pop = pop then (evolve iter-pop rule)
+	  append (draw-pop-gen iter-pop i))))
+
+(defun ca (rule)
+  "Draw a 1d cellular automata in a new *ca* buffer, following RULE."
+  (interactive "nRule: ")
+  (switch-to-buffer "*ca*")
+  (erase-buffer)
+  (let ((svg (svg-create (window-pixel-width) (window-pixel-height)))
+	(max-gens (/ (window-pixel-height) cell-size))
+	(rule-arr (make-rule rule)))
+    (setq svg (append svg (draw-rule svg rule-arr max-gens)))
+    (svg-insert-image svg)
+    nil))
