830bf590efafc7192c2d51fb56893d7a59d1f5b6
Tom Hutchings <tom@toms-mbp.home>
2020-11-09 10:07:14 +0000

    Org publishing, IRC, Posframe, better dashboard.

--- custom.el
+++ custom.el
@@ -6,9 +6,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
+   '("556200ac57b155fb0cf5997f94af4055fa3b845068cc6fb4cb0d8af202aa77da" "1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
  '(org-agenda-files
-   '("~/doc/org/camping.org" "~/doc/org/cars.org" "~/doc/org/clocktables.org" "~/doc/org/coffee.org" "~/doc/org/cw.org" "~/doc/org/done.org" "~/doc/org/hifi.org" "~/doc/org/house.org" "~/doc/org/houses.org" "~/doc/org/inbox.org" "~/doc/org/jobs.org" "~/doc/org/languages.org" "~/doc/org/media.org" "~/doc/org/money.org" "~/doc/org/moto.org" "~/doc/org/personal.org" "~/doc/org/projects.org" "~/doc/org/shopping-list.org" "~/doc/org/someday.org" "~/doc/org/todo.org")))
+   '("~/doc/org/camping.org" "~/doc/org/cars.org" "~/doc/org/clocktables.org" "~/doc/org/coffee.org" "~/doc/org/cw.org" "~/doc/org/done.org" "~/doc/org/hifi.org" "~/doc/org/house.org" "~/doc/org/houses.org" "~/doc/org/inbox.org" "~/doc/org/jobs.org" "~/doc/org/languages.org" "~/doc/org/media.org" "~/doc/org/money.org" "~/doc/org/moto.org" "~/doc/org/next.org" "~/doc/org/personal.org" "~/doc/org/projects.org" "~/doc/org/shopping-list.org" "~/doc/org/someday.org" "~/doc/org/todo.org")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
