957de76bfc1a844d6159fd1c1abc9abb485bedf3
Tom Hutchings <tom.a.hutchings@gmail.com>
2020-11-10 10:02:33 +0000

    Tidy up work config.

--- custom.el
+++ custom.el
@@ -6,7 +6,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default)
+   '("1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
+ '(org-agenda-files
+   '("~/doc/org/bro.org" "~/doc/org/done.org" "~/doc/org/inbox.org" "~/doc/org/todo.org")))
 
 
 (custom-set-faces
@@ -14,4 +16,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ET Book" :height 140 :weight light)))))
+ )
