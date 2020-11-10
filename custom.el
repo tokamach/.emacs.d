c7ab567b3b162520e1dce06ec8e8d2de2b3f2dc6
Tom Hutchings <tom@toms-mbp.home>
2020-11-10 09:59:55 +0000

    New dashboard pics, fix variable pitch font, etc...

--- custom.el
+++ custom.el
@@ -15,4 +15,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ET Book" :height 140 :weight light)))))
+ '(variable-pitch ((t (:family "ETBembo")))))
