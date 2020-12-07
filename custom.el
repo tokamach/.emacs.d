aef0ee291901d511fea97acd5f6e789381ec684a
Tom Hutchings <tom@toms-mbp.home>
2020-12-07 12:45:43 +0000

    Org variable pitch and some misc stuff.

--- custom.el
+++ custom.el
@@ -15,4 +15,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ETBembo")))))
+ '(variable-pitch ((t (:family "ETBembo" :height 1.25)))))
