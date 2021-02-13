3f63dfb70321c288f5aaf7f30290af052cc2f504
Tom Hutchings <tom@toms-mbp.home>
2021-02-13 18:22:54 +0000

    Font, user dir, icomplete, matrix.

--- custom.el
+++ custom.el
@@ -6,13 +6,36 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("556200ac57b155fb0cf5997f94af4055fa3b845068cc6fb4cb0d8af202aa77da" "1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
+   '("6fc5c5d388fb99737bdeee3aac74e660b4cf2ef39d6354cdbf1e7c8e475a224e" "556200ac57b155fb0cf5997f94af4055fa3b845068cc6fb4cb0d8af202aa77da" "1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
+ '(fci-rule-color "#292617")
+ '(minions-mode t)
  '(org-agenda-files
-   '("~/doc/org/camping.org" "~/doc/org/cars.org" "~/doc/org/clocktables.org" "~/doc/org/coffee.org" "~/doc/org/cw.org" "~/doc/org/done.org" "~/doc/org/hifi.org" "~/doc/org/house.org" "~/doc/org/houses.org" "~/doc/org/inbox.org" "~/doc/org/jobs.org" "~/doc/org/languages.org" "~/doc/org/media.org" "~/doc/org/money.org" "~/doc/org/moto.org" "~/doc/org/next.org" "~/doc/org/personal.org" "~/doc/org/projects.org" "~/doc/org/shopping-list.org" "~/doc/org/someday.org" "~/doc/org/todo.org")))
+   '("~/doc/org/camping.org" "~/doc/org/cars.org" "~/doc/org/clocktables.org" "~/doc/org/coffee.org" "~/doc/org/cw.org" "~/doc/org/done.org" "~/doc/org/hifi.org" "~/doc/org/house.org" "~/doc/org/houses.org" "~/doc/org/inbox.org" "~/doc/org/jobs.org" "~/doc/org/languages.org" "~/doc/org/media.org" "~/doc/org/money.org" "~/doc/org/moto.org" "~/doc/org/next.org" "~/doc/org/personal.org" "~/doc/org/projects.org" "~/doc/org/shopping-list.org" "~/doc/org/someday.org" "~/doc/org/todo.org" "~/doc/org/travel.org"))
+ '(vc-annotate-background "#201D0E")
+ '(vc-annotate-color-map
+   '((20 . "#737063")
+     (40 . "#B0ADA2")
+     (60 . "#B0ADA2")
+     (80 . "#D5D2C8")
+     (100 . "#D5D2C8")
+     (120 . "#ECE9E0")
+     (140 . "#ECE9E0")
+     (160 . "#FAF7EE")
+     (180 . "#FAF7EE")
+     (200 . "#FAF7EE")
+     (220 . "#FFFFF8")
+     (240 . "#FFFFF8")
+     (260 . "#FFFFF8")
+     (280 . "#FFFFFD")
+     (300 . "#FFFFFD")
+     (320 . "#FFFFFD")
+     (340 . "#FFFFFF")
+     (360 . "#FFFFFF")))
+ '(vc-annotate-very-old-color "#ECE9E0"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ETBembo" :height 1.25)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 1)))))
