2ec845b280f5f08900b24dc77b52c4ab7437d2dd
Tom Hutchings <tom@toms-mbp.home>
2021-07-03 16:56:50 +0100

    Org habit, roam, register richii submodule

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(fci-rule-color "#292617")
  '(minions-mode t)
  '(org-agenda-files
-   '("~/doc/org/camping.org" "~/doc/org/cars.org" "~/doc/org/clocktables.org" "~/doc/org/coffee.org" "~/doc/org/cw.org" "~/doc/org/done.org" "~/doc/org/hifi.org" "~/doc/org/house.org" "~/doc/org/houses.org" "~/doc/org/inbox.org" "~/doc/org/jobs.org" "~/doc/org/languages.org" "~/doc/org/media.org" "~/doc/org/money.org" "~/doc/org/moto.org" "~/doc/org/next.org" "~/doc/org/personal.org" "~/doc/org/projects.org" "~/doc/org/shopping-list.org" "~/doc/org/someday.org" "~/doc/org/todo.org" "~/doc/org/travel.org"))
+   '("/Users/tom/doc/org/camping.org" "/Users/tom/doc/org/cars.org" "/Users/tom/doc/org/clocktables.org" "/Users/tom/doc/org/coffee.org" "/Users/tom/doc/org/cw.org" "/Users/tom/doc/org/done.org" "/Users/tom/doc/org/habit.org" "/Users/tom/doc/org/hifi.org" "/Users/tom/doc/org/house.org" "/Users/tom/doc/org/inbox.org" "/Users/tom/doc/org/jobs.org" "/Users/tom/doc/org/languages.org" "/Users/tom/doc/org/media.org" "/Users/tom/doc/org/misc.org" "/Users/tom/doc/org/money.org" "/Users/tom/doc/org/moto.org" "/Users/tom/doc/org/next.org" "/Users/tom/doc/org/personal.org" "/Users/tom/doc/org/projects.org" "/Users/tom/doc/org/running.org" "/Users/tom/doc/org/shopping-list.org" "/Users/tom/doc/org/someday.org" "/Users/tom/doc/org/todo.org" "/Users/tom/doc/org/travel.org"))
  '(vc-annotate-background "#201D0E")
  '(vc-annotate-color-map
    '((20 . "#737063")
@@ -38,4 +38,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ETBembo" :height 1)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 1.1)))))
