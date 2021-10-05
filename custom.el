e0c75b3a8d9d9d5085db2bfb1094d7a1a0be48e7
Tom Hutchings <tom@toms-mbp.home>
2021-10-05 21:10:59 +0100

    Clean up unused stuff in conf a little

--- custom.el
+++ custom.el
@@ -6,36 +6,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("6fc5c5d388fb99737bdeee3aac74e660b4cf2ef39d6354cdbf1e7c8e475a224e" "556200ac57b155fb0cf5997f94af4055fa3b845068cc6fb4cb0d8af202aa77da" "1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default))
- '(fci-rule-color "#292617")
- '(minions-mode t)
- '(org-agenda-files
-   '("/Users/tom/doc/org/camping.org" "/Users/tom/doc/org/cars.org" "/Users/tom/doc/org/clocktables.org" "/Users/tom/doc/org/coffee.org" "/Users/tom/doc/org/cw.org" "/Users/tom/doc/org/done.org" "/Users/tom/doc/org/habit.org" "/Users/tom/doc/org/hifi.org" "/Users/tom/doc/org/house.org" "/Users/tom/doc/org/inbox.org" "/Users/tom/doc/org/jobs.org" "/Users/tom/doc/org/languages.org" "/Users/tom/doc/org/media.org" "/Users/tom/doc/org/misc.org" "/Users/tom/doc/org/money.org" "/Users/tom/doc/org/moto.org" "/Users/tom/doc/org/next.org" "/Users/tom/doc/org/personal.org" "/Users/tom/doc/org/projects.org" "/Users/tom/doc/org/running.org" "/Users/tom/doc/org/shopping-list.org" "/Users/tom/doc/org/someday.org" "/Users/tom/doc/org/todo.org" "/Users/tom/doc/org/travel.org"))
- '(vc-annotate-background "#201D0E")
- '(vc-annotate-color-map
-   '((20 . "#737063")
-     (40 . "#B0ADA2")
-     (60 . "#B0ADA2")
-     (80 . "#D5D2C8")
-     (100 . "#D5D2C8")
-     (120 . "#ECE9E0")
-     (140 . "#ECE9E0")
-     (160 . "#FAF7EE")
-     (180 . "#FAF7EE")
-     (200 . "#FAF7EE")
-     (220 . "#FFFFF8")
-     (240 . "#FFFFF8")
-     (260 . "#FFFFF8")
-     (280 . "#FFFFFD")
-     (300 . "#FFFFFD")
-     (320 . "#FFFFFD")
-     (340 . "#FFFFFF")
-     (360 . "#FFFFFF")))
- '(vc-annotate-very-old-color "#ECE9E0"))
+   '("801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "6fc5c5d388fb99737bdeee3aac74e660b4cf2ef39d6354cdbf1e7c8e475a224e" "556200ac57b155fb0cf5997f94af4055fa3b845068cc6fb4cb0d8af202aa77da" "1cefd82df66eb0ad6cd3fd5baa1b684935ad88478f9ca40f74bcf080dac691b3" default)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "ETBembo" :height 1.1)))))
